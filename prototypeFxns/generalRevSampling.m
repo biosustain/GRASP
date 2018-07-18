40ab67d592c0a43f3fe6a70afb17157ba4ea72b8
Pedro Saa <32940774+SysBioEng@users.noreply.github.com>
2018-07-18 23:58:06 +0200

    Add files via upload
    A couple of new fxns and tests for thermodynamic calculations and reversibilities for promiscuous enzymes.

---
+++ prototypeFxns/generalRevSampling.m
@@ -0,0 +1,93 @@
+function Rev = generalRevSampling(Omega,DGr_RT,numSamples,thinning,burn_in)
+% Thermodynamic-based Flux Balance Analysis
+% 
+% ------------------ Pedro Saa 2018 ---------------------------------------
+%
+% Check input parameters
+if (nargin<2)
+    disp('Not enough input parameters'); return;
+elseif (nargin<3)
+    numSamples = 1;
+    thinning   = 1e1;
+    burn_in    = 1e3;
+elseif (nargin<4)
+    thinning   = 1e1;
+    burn_in    = 1e3;
+elseif (nargin<5)
+    burn_in    = 1e3;    
+end
+
+% General parameters
+uTol = 1e-6;
+
+% Define projection parameters
+A  = Omega;
+b  = DGr_RT;
+xcurr = pinv(A)*b;
+
+% General solutions is of the form x = N*alpha + xp. Let us first calculate
+% a basis for null(A)
+N = null(A);
+
+% Find enclosing box for the extended space
+Aeq = A;
+beq = b;
+lb  = min(DGr_RT)*ones(size(Omega,2),1);
+ub  = zeros(size(Omega,2),1);
+f   = zeros(size(N,1),1);
+x_bnd = zeros(size(N,1),2);
+for ix = 1:numel(f)
+    
+    % Solve min problem
+    f(ix) = 1;
+    [~,fval] = linprog(f,[],[],Aeq,beq,lb,ub);
+    x_bnd(ix,1) = fval;
+    
+    % Solve max problem
+    f(ix) = -1;
+    [~,fval] = linprog(f,[],[],Aeq,beq,lb,ub);
+    x_bnd(ix,2) = -fval;
+    
+    % Reset objective
+    f(ix) = 0;
+end
+
+% Figure out the true max & min step sizes
+counter = 0;
+xcount  = 0;
+Rev     = zeros(size(N,1),numSamples);
+while counter < burn_in + numSamples*thinning
+    
+    % Advance one step
+    counter = counter+1;
+    
+    % Sample direction
+    udir = N(:,randi(size(N,2)));
+    
+    % Figure out where to move
+    posDir      = bsxfun(@gt,udir,uTol);
+    negDir      = bsxfun(@lt,udir,-uTol);
+    distUb      = bsxfun(@plus,-xcurr,x_bnd(:,2));
+    distLb      = bsxfun(@minus,xcurr,x_bnd(:,1));
+    posStepTemp = bsxfun(@rdivide,distUb,udir);
+    negStepTemp = bsxfun(@rdivide,-distLb,udir);
+    
+    % Figure out step
+    posStep = min([posStepTemp(posDir);negStepTemp(negDir)],[],1);
+    negStep = max([posStepTemp(negDir);negStepTemp(posDir)],[],1);
+    
+    % Generate new sample vector
+    lambda = negStep + rand(1)*(posStep-negStep);
+    xcurr  = xcurr + lambda*udir;
+    
+    % Save new point
+    if (burn_in<=counter) && ~mod(counter-burn_in,thinning)
+        xcount = xcount + 1;
+        Rev(:,xcount) = xcurr;
+    end
+end
+Rev = exp(Rev);       % Return actual reversibilities (not log)
+
+% % Find proportion of reversibilities outside the hypercube
+% bad_revs = sum((any(x>0,1) + any(x<min(DGr_RT),1))>0);
+% disp(['Number of badly calculated instances: ',num2str(bad_revs)]);
---
+++ prototypeFxns/test_Rsampling.m
@@ -0,0 +1,114 @@
+% Test general sampling algorithm for reversibilities 18/07/18
+clc,clearvars,close all
+
+% Number of samples
+numSamples = 5e4;
+thinning   = 1;
+
+%% Example 1
+% Linear mechanism: i) A <-> P
+Omega1  = [1,1,1];
+DGr1    = -10;                  % [kJ/mol]
+RT      = 8.314*298.15/1e3;     % [kJ/mol]
+Rev_1   = generalRevSampling(Omega1,DGr1/RT,numSamples,thinning);
+
+%% Example 2
+% 2 linear promiscuous mechanisms without common steps:
+% i) A1 <-> P1 & ii) A2 <-> P2 
+Omega2  = [1,1,1.05,0,0,0;...
+          0,0,0,1,1,1];
+DGr2    = [-10;-5];             % [kJ/mol]
+Rev_2   = generalRevSampling(Omega2,DGr2/RT,numSamples,thinning);
+
+%% Example 3
+% Linear promiscuous mechanism with 1 common step:
+% i) A <-> P1 & ii) A <-> P2
+Omega3  = [1,1,1.05,0,0;...
+          1,0,0,1,1];
+DGr3    = [-10;-8];             % [kJ/mol]
+Rev_3    = generalRevSampling(Omega3,DGr3/RT,numSamples,thinning);
+
+%% Example 4
+% Linear promiscuous mechanism with 1 common step and 1 inhibitor:
+% i) A <-> P1 & ii) A <-> P2
+Omega4  = [1,1,1.05,0,0,0;...
+          1,0,0,1,1,0;...
+          0,0,0,0,0,1];
+DGr4    = [-10;-8;0];           % [kJ/mol]
+Rev_4   = generalRevSampling(Omega4,DGr4/RT,numSamples,thinning);
+
+%% Compare sampling distributions for example 1 (where we know how the distribution looks like - example 2 is also possible)
+% Plot Results Example 1
+rev1 = randg(1,size(Omega1,2),numSamples,thinning);
+rev1 = rev1./repmat(sum(rev1,1),size(Omega1,2),1);
+rev1 = exp(rev1*(DGr1/RT));
+figure (1)
+xbin = linspace(.05,1,15);
+for ix = 1:3
+    subplot(2,3,ix)
+    f = hist(rev1(ix,:),xbin);
+    y = f/trapz(xbin,f);
+    bar(xbin,y)
+    if (ix == 1)
+        ylabel('Probability density')
+    end
+    if (ix == 2)
+        title(['Correct distribution: \mu = ',num2str(mean(rev1(ix,:))),', \sigma = ',num2str(std(rev1(ix,:)))])
+    else
+        title(['\mu = ',num2str(mean(rev1(ix,:))),', \sigma = ',num2str(std(rev1(ix,:)))])
+    end
+    axis([0,1.05,0,1.2*max(y)])
+    subplot(2,3,ix+3)
+    f = hist(Rev_1(ix,:),xbin);
+    y = f/trapz(xbin,f);
+    bar(xbin,y)
+    if (ix == 1)
+        ylabel('Probability density')        
+    end
+    if (ix == 2)
+        title(['Distribution new method: \mu = ',num2str(mean(Rev_1(ix,:))),', \sigma = ',num2str(std(Rev_1(ix,:)))])
+    else
+        title(['\mu = ',num2str(mean(Rev_1(ix,:))),', \sigma = ',num2str(std(Rev_1(ix,:)))])
+    end
+    axis([0,1.05,0,1.2*max(y)])
+    xlabel(['R_',num2str(ix)])
+end
+
+% Plot Results Example 3
+figure (2)
+for ix = 1:5
+    subplot(2,3,ix)
+    f = hist(Rev_3(ix,:),xbin);
+    y = f/trapz(xbin,f);
+    bar(xbin,y)
+    if (ix == 1) || (ix == 4)
+        ylabel('Probability density')
+    end
+    if (ix==2)
+       title(['Distribution new method: \mu = ',num2str(mean(Rev_3(ix,:))),', \sigma = ',num2str(std(Rev_3(ix,:)))])
+    else
+       title(['\mu = ',num2str(mean(Rev_3(ix,:))),', \sigma = ',num2str(std(Rev_3(ix,:)))]) 
+    end    
+    axis([0,1.05,0,1.2*max(y)])
+    xlabel(['R_',num2str(ix)])
+end
+
+% Plot Results Example 4
+figure (3)
+for ix = 1:6
+    subplot(2,3,ix)
+    f = hist(Rev_4(ix,:),xbin);
+    y = f/trapz(xbin,f);
+    bar(xbin,y)
+    if (ix == 1) || (ix == 4)
+        ylabel('Probability density')
+    end
+    if (ix==2)
+        title(['Distribution new method: \mu = ',num2str(mean(Rev_4(ix,:))),', \sigma = ',num2str(std(Rev_4(ix,:)))])
+    else
+        title(['\mu = ',num2str(mean(Rev_4(ix,:))),', \sigma = ',num2str(std(Rev_4(ix,:)))])
+    end
+    title(['\mu = ',num2str(mean(Rev_4(ix,:))),', \sigma = ',num2str(std(Rev_4(ix,:)))])
+    axis([0,1.05,0,1.2*max(y)])
+    xlabel(['R_',num2str(ix)])
+end
