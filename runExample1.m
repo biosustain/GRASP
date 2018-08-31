415bc23e433686a420d0f9ca79f606b1cb087237
Pedro Saa <32940774+SysBioEng@users.noreply.github.com>
2018-08-31 12:13:55 -0300

    Add files via upload

--- runExample1.m
+++ runExample1.m
@@ -6,60 +6,77 @@
 %
 % Outputs:      (-)
 %--------------------- Pedro Saa 2017 -------------------------------------
-profile on
 rng('default');																											% for reproducibility
 delete(gcp('nocreate'));       				            																% check first that no other process is running
-addpath('./patternFxns','./ensembleFxns','./reactions_MEP');
+addpath('./patternFxns','./ensembleFxns');
 
 % 1. Load information
 iter     = 1;
 popIdx   = 1;
-%ensemble = loadEnsembleStructure('models/input/melatonin_pathway_no_transporters_HMP1489');		% This line loads the model in the Excel file
-ensemble = loadEnsembleStructure('models/input/MEP_example');		% This line loads the model in the Excel file
-%load('ensembleStarter.mat','ensemble');
+ensemble = loadEnsembleStructure('input_test/MEP_example');           % Here the test case MEP pathway model is chosen
+% ensemble = loadEnsembleStructure('input_test/HMP1489_r1_t0_profile');   % Here the test case HMP is chosen
 
 % 2. Initialize and perform rejection sampling
 ensemble = initializeEnsemble(ensemble,popIdx,1);
+addKineticFxnsToPath(ensemble);
 
 % Check whether the job is ran in parallel
-disp('Running rejection sampler. Population 1.');
+disp('Running initial sampler.');
 
 % Setup folder with temp files
 try
-	rmdir('tempRejection','s');
-	mkdir('tempRejection');
+    rmdir('temp','s');
+    mkdir('temp');
 catch
-	mkdir('tempRejection');
-end
-	
-% Preallocate memory for the remaing fields in the ensemble structure
-tolScore = zeros(ensemble.replenishedParticles(popIdx),1);
-strucIdx = zeros(ensemble.replenishedParticles(popIdx),1);    
-xopt{ensemble.replenishedParticles(popIdx),1}      = [];
-simFluxes{ensemble.replenishedParticles(popIdx),1} = [];
-    
-% Initiate progress report	
-progress = zeros(5,1);	
-save progress.txt -ascii progress;    
-if ensemble.parallel
-		parpool(ensemble.numCores);																								% Initiate parallel pool and run parallel foor loop    
-	parfor ix = 1:ensemble.numParticles
-		rng('shuffle');
-		[models(ix),strucIdx(ix),xopt{ix},tolScore(ix),simFluxes{ix}] = rejectionSampler(ensemble);
-	end
-	delete(gcp);
-else
-	for ix = 1:ensemble.numParticles
-		[models(ix),strucIdx(ix),xopt{ix},tolScore(ix),simFluxes{ix}] = rejectionSampler(ensemble);
-	end
+    mkdir('temp');
 end
 
-% Append active particles from the previous population to the new ones
+% Preallocate memory for the remaing fields in the ensemble structure
+tolScore = zeros(ensemble.replenishedParticles(popIdx),1);
+strucIdx = zeros(ensemble.replenishedParticles(popIdx),1);
+xopt{ensemble.replenishedParticles(popIdx),1}      = [];
+simFluxes{ensemble.replenishedParticles(popIdx),1} = [];
+
+% Figure out the sampling mode
+if ~strcmpi(ensemble.sampler,'ORACLE')
+    
+    % Initiate progress report for rejection sampling
+    progress = zeros(5,1);
+    save progress.txt -ascii progress;
+    if ensemble.parallel
+        parpool(ensemble.numCores);																								% Initiate parallel pool and run parallel foor loop
+        parfor ix = 1:ensemble.numParticles
+            rng('shuffle');
+            [models(ix),strucIdx(ix),xopt{ix},tolScore(ix),simFluxes{ix}] = initialSampler(ensemble);
+        end
+        delete(gcp);
+    else
+        for ix = 1:ensemble.numParticles
+            [models(ix),strucIdx(ix),xopt{ix},tolScore(ix),simFluxes{ix}] = initialSampler(ensemble);
+        end
+    end
+    
+    % In the ORACLE mode we are only interested in the models
+else
+    if ensemble.parallel
+        parpool(ensemble.numCores);																								% Initiate parallel pool and run parallel foor loop
+        parfor ix = 1:ensemble.numParticles
+            rng('shuffle');
+            models(ix) = initialSampler(ensemble);
+        end
+        delete(gcp);
+    else
+        for ix = 1:ensemble.numParticles
+            models(ix) = initialSampler(ensemble);
+        end
+    end
+end
+
+% Append sampling results
 ensemble.populations(1).strucIdx  = strucIdx;                                                                           % model structures
 ensemble.populations(1).tolScore  = tolScore;                                                                           % tolerance score
 ensemble.populations(1).xopt      = xopt;                                                                               % optimal value found
 ensemble.populations(1).simFluxes = simFluxes;                                                                          % simulated fluxes
-ensemble.populations(1).models    = models;                                                                             % model particles    
+ensemble.populations(1).models    = models;                                                                             % model particles
 clearvars -except ensemble popIdx iter
-save('models/output/ensembleSMC_rejection_MEP.mat');
-profsave
+save('output_test/ensembleSMC_rejection_MEP.mat');
