3ed4f794d5a13db37939acc8bc449b2da82d9bdd
Pedro Saa <32940774+SysBioEng@users.noreply.github.com>
2018-10-09 18:20:41 -0300

    Update buildKineticFxn.m

--- patternFxns/buildKineticFxn.m
+++ patternFxns/buildKineticFxn.m
@@ -25,8 +25,8 @@ fprintf(fid,['v = zeros(',num2str(size(ensemble.Sred,2)),',',num2str(totalEvals)
 fprintf(fid,['E = zeros(',num2str(size(ensemble.Sred,2)),',',num2str(totalEvals),');\n']);      % Preallocation of memory (enz)
 fprintf(fid,['x = [x,x(:,ones(1,',num2str(totalEvals-1),')) + diag(h*1i*ones(',num2str(totalEvals-1),',1))];\n']);      % Preallocation of memory (free vars)                                                                                               
 fprintf(fid,'else\n');
-fprintf(fid,['v = zeros(',num2str(size(ensemble.Sred,2)),',size(x,2);\n']);      % Preallocation of memory (rxns)
-fprintf(fid,['E = zeros(',num2str(size(ensemble.Sred,2)),',size(x,2);\n']);      % Preallocation of memory (enz)
+fprintf(fid,['v = zeros(',num2str(size(ensemble.Sred,2)),',size(x,2));\n']);      % Preallocation of memory (rxns)
+fprintf(fid,['E = zeros(',num2str(size(ensemble.Sred,2)),',size(x,2));\n']);      % Preallocation of memory (enz)
 fprintf(fid,'end\n');
 
 % Define metabolite species
