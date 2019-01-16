7a1b5a57c7b3e84be68ae9baad7564231303485d
martamatos <marta.ra.matos@gmail.com>
2019-01-16 16:54:06 +0100

    Fixed issue with fixedExchange mechanism on sampleGeneraalReversibilities

---
+++ run_tests.m
@@ -0,0 +1,116 @@
+% Example 1. Sample a kinetic model of the MEP pathway (reference point)
+%--------------------------------------------------------------------------
+% Executes GRASP workflow
+%
+% Inputs:       (-)
+%
+% Outputs:      (-)
+%--------------------- Pedro Saa 2017 -------------------------------------
+clear
+rng('default');																											% for reproducibility
+delete(gcp('nocreate'));       				            																% check first that no other process is running
+addpath('./patternFxns','./ensembleFxns');
+
+% 1. Load information
+iter     = 1;
+popIdx   = 1;
+
+ensemble = loadEnsembleStructure('input_test/GRASP_input_yeast');           % Here the test case HMP pathway model is chosen
+
+% 2. Initialize and perform rejection sampling
+ensemble = initializeEnsemble(ensemble,popIdx,1);
+addKineticFxnsToPath(ensemble);
+
+disp('GRASP_input_yeast ran fine');
+
+clear
+rng('default');																											% for reproducibility
+delete(gcp('nocreate'));       				            																% check first that no other process is running
+addpath('./patternFxns','./ensembleFxns');
+
+% 1. Load information
+iter     = 1;
+popIdx   = 1;
+
+ensemble = loadEnsembleStructure('input_test/MEP_example');           % Here the test case HMP pathway model is chosen
+
+% 2. Initialize and perform rejection sampling
+ensemble = initializeEnsemble(ensemble,popIdx,1);
+addKineticFxnsToPath(ensemble);
+
+disp('MEP_example ran fine');
+
+
+clear
+rng('default');																											% for reproducibility
+delete(gcp('nocreate'));       				            																% check first that no other process is running
+addpath('./patternFxns','./ensembleFxns');
+
+% 1. Load information
+iter     = 1;
+popIdx   = 1;
+
+ensemble = loadEnsembleStructure('input_test/MEP_example_inhibitors');           % Here the test case HMP pathway model is chosen
+
+% 2. Initialize and perform rejection sampling
+ensemble = initializeEnsemble(ensemble,popIdx,1);
+addKineticFxnsToPath(ensemble);
+
+disp('MEP_example_inhibitors ran fine');
+
+
+
+
+clear
+rng('default');																											% for reproducibility
+delete(gcp('nocreate'));       				            																% check first that no other process is running
+addpath('./patternFxns','./ensembleFxns');
+
+% 1. Load information
+iter     = 1;
+popIdx   = 1;
+
+ensemble = loadEnsembleStructure('input_test/HMP1489_r1_t0');           % Here the test case HMP pathway model is chosen
+
+% 2. Initialize and perform rejection sampling
+ensemble = initializeEnsemble(ensemble,popIdx,1);
+addKineticFxnsToPath(ensemble);
+
+disp('HMP1489_r1_t0 ran fine');
+
+
+
+clear
+rng('default');																											% for reproducibility
+delete(gcp('nocreate'));       				            																% check first that no other process is running
+addpath('./patternFxns','./ensembleFxns');
+
+% 1. Load information
+iter     = 1;
+popIdx   = 1;
+
+ensemble = loadEnsembleStructure('input_test/HMP1489_r1_t0_test_gibbs');           % Here the test case HMP pathway model is chosen
+
+% 2. Initialize and perform rejection sampling
+ensemble = initializeEnsemble(ensemble,popIdx,1);
+addKineticFxnsToPath(ensemble);
+
+disp('HMP1489_r1_t0_test_gibbs ran fine');
+
+
+clear
+rng('default');																											% for reproducibility
+delete(gcp('nocreate'));       				            																% check first that no other process is running
+addpath('./patternFxns','./ensembleFxns');
+
+% 1. Load information
+iter     = 1;
+popIdx   = 1;
+
+ensemble = loadEnsembleStructure('input_test/HMP1489_r1_t0_inhibitors');           % Here the test case HMP pathway model is chosen
+
+% 2. Initialize and perform rejection sampling
+ensemble = initializeEnsemble(ensemble,popIdx,1);
+addKineticFxnsToPath(ensemble);
+
+disp('HMP1489_r1_t0_inhibitors ran fine');
