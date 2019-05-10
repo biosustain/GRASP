69651b84c9ad7bf63b72f720712f981d5028f88f
martamatos <marta.ra.matos@gmail.com>
2019-05-10 14:27:33 +0200

    Final test reorganization

--- run_tests.m
+++ run_tests.m
@@ -79,3 +79,24 @@ addKineticFxnsToPath(ensemble);
 disp('HMP2360_r0_t0_nick ran fine');
 
 
+% This last tests throws an error on purpose, the error message should be:
+% "The TMFA problem is infeasible. Verify that the standard Gibbs free energy and
+% metabolite concentration values are valid/correct. Reactions
+% r_AANAT, r_AANAT_tryptm with standard Gibbs energies'[100,200][10,20]' seem to be the problem."
+
+clear
+rng('default');																											% for reproducibility
+delete(gcp('nocreate'));       				            																% check first that no other process is running
+addpath('./patternFxns','./ensembleFxns');
+
+% 1. Load information
+iter     = 1;
+popIdx   = 1;
+
+ensemble = loadEnsembleStructure('input_test/HMP1489_r1_t0_test_gibbs');           % Here the test case HMP pathway model is chosen
+
+% 2. Initialize and perform rejection sampling
+ensemble = initializeEnsemble(ensemble,popIdx,1);
+addKineticFxnsToPath(ensemble);
+
+disp('HMP1489_r1_t0_test_gibbs ran fine');
