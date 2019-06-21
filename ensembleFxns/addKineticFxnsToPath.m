2d27c11eaf3b4595c0453e6ef4c695346d8a569e
martamatos <marta.ra.matos@gmail.com>
2019-06-21 14:09:10 +0200

    Feat: added function to build model ensemble

--- ensembleFxns/addKineticFxnsToPath.m
+++ ensembleFxns/addKineticFxnsToPath.m
@@ -4,7 +4,7 @@ function addKineticFxnsToPath(ensemble)
 % ------------------ Pedro Saa, Marta Matos 2018 ---------------------------------------
 %
 rootName =  strcat('./reactions_', ensemble.description, '_');
-disp(rootName);
+
 for ix = 1:ensemble.numStruct
     tmpName = [rootName,num2str(ix)];
     addpath(tmpName);
