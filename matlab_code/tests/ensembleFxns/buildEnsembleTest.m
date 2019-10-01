b0956dbf72d70e5420e97f9169df7e6a2a0c8832
martamatos <marta.ra.matos@gmail.com>
2019-10-01 14:06:09 +0200

    Test: included integration tests into the unit tests
    All tests that only include loading and initializing
    the ensemble went into initializeEnsembleTest.
    The tests that involved building the ensemble
    went into buildEnsembleTest.

--- matlab_code/tests/ensembleFxns/buildEnsembleTest.m
+++ matlab_code/tests/ensembleFxns/buildEnsembleTest.m
@@ -13,10 +13,14 @@ classdef buildEnsembleTest < matlab.unittest.TestCase
     methods(TestMethodTeardown)
         function removeReactionsFolder(testCase)           
 
-            reactionsFolderList = {fullfile(testCase.currentPath{1}, '..', '..', '..', 'reactions', 'toy_model1_random2_1'),
-                                   fullfile(testCase.currentPath{1}, '..', '..', '..', 'reactions', 'toy_model1_allosteric2_1')};
+            reactionsFolderList = {fullfile(testCase.currentPath{1}, '..', '..', '..', 'reactions', 'toy_model1_random2_1'), ...
+                                   fullfile(testCase.currentPath{1}, '..', '..', '..', 'reactions', 'toy_model1_allosteric2_1'), ...
+                                   fullfile(testCase.currentPath{1}, '..', '..', '..', 'reactions', 'toy_model1_new_1'), ...
+                                   fullfile(testCase.currentPath{1}, '..', '..', '..', 'reactions', 'toy_model1_no_promiscuous2_1'), ...
+                                   fullfile(testCase.currentPath{1}, '..', '..', '..', 'reactions', 'toy_model5_debug_1'), ...
+                                   fullfile(testCase.currentPath{1}, '..', '..', '..', 'reactions', 'toy_model5_dGs_1')};
             
-            for i = 1:size(reactionsFolderList, 1)
+            for i = 1:size(reactionsFolderList, 2)
                 if exist(reactionsFolderList{i}, 'dir')
                     rmdir(reactionsFolderList{i}, 's');
                 end
@@ -64,8 +68,89 @@ classdef buildEnsembleTest < matlab.unittest.TestCase
                    
             testCase.verifyEqual(trueRes, ensemble);
         end
-       
-    end
+        
+        function testBuildEnsembleAllStable(testCase)
+            % All models are expected to be stable
+            
+            seed = 1;
+            rng(seed)
+            
+            modelID = 'toy_model1_new';
+            inputFile = fullfile(testCase.currentPath{1}, 'testFiles', modelID);
+            outputFile = fullfile(testCase.currentPath{1}, 'testFiles', [modelID, '.mat']);
+            
+            maxNumberOfSamples = 1000;
+            eigThreshold = 10^-5;
+            
+            ensemble = buildEnsemble(inputFile,outputFile,maxNumberOfSamples,eigThreshold);
+
+            trueRes = load(fullfile(testCase.currentPath{1}, 'testFiles', 'trueResBuildEnsembleAllStable'));
+            trueRes = trueRes.ensemble;
+                   
+            testCase.verifyEqual(trueRes, ensemble);
+        end
+        
+        function testBuildEnsembleNoPromiscuous(testCase)
+            % 10-20% of all models are expected to be unstable
+            
+            seed = 1;
+            rng(seed)
+            
+            modelID = 'toy_model1_no_promiscuous2';
+            inputFile = fullfile(testCase.currentPath{1}, 'testFiles', modelID);
+            outputFile = fullfile(testCase.currentPath{1}, 'testFiles', [modelID, '.mat']);
+            
+            maxNumberOfSamples = 1000;
+            eigThreshold = 10^-5;
+            
+            ensemble = buildEnsemble(inputFile,outputFile,maxNumberOfSamples,eigThreshold);
+          
+            trueRes = load(fullfile(testCase.currentPath{1}, 'testFiles', 'trueResBuildEnsembleNoPromiscuous'));
+            trueRes = trueRes.ensemble;
+                   
+            testCase.verifyEqual(trueRes, ensemble);
+        end
+        
+        function testBuildEnsembleLargeModel(testCase)
+            
+            seed = 1;
+            rng(seed)
+            
+            modelID = 'toy_model5_debug';
+            inputFile = fullfile(testCase.currentPath{1}, 'testFiles', modelID);
+            outputFile = fullfile(testCase.currentPath{1}, 'testFiles', [modelID, '.mat']);
+            
+            maxNumberOfSamples = 1000;
+            eigThreshold = 10^-5;
+            
+            ensemble = buildEnsemble(inputFile,outputFile,maxNumberOfSamples,eigThreshold);
+
+            trueRes = load(fullfile(testCase.currentPath{1}, 'testFiles', 'trueResBuildEnsembleLargeModel'));
+            trueRes = trueRes.ensemble;
+                   
+            testCase.verifyEqual(trueRes, ensemble);
+        end
+        
+        function testBuildEnsembleLargeModeldGs(testCase)
+            
+            seed = 1;
+            rng(seed)
+            
+            modelID = 'toy_model5_dGs';
+            inputFile = fullfile(testCase.currentPath{1}, 'testFiles', modelID);
+            outputFile = fullfile(testCase.currentPath{1}, 'testFiles', [modelID, '.mat']);
+            
+            maxNumberOfSamples = 1000;
+            eigThreshold = 10^-5;
+            
+            ensemble = buildEnsemble(inputFile,outputFile,maxNumberOfSamples,eigThreshold);
+
+            trueRes = load(fullfile(testCase.currentPath{1}, 'testFiles', 'trueResBuildEnsembleLargeModeldGs'));
+            trueRes = trueRes.ensemble;
+                   
+            testCase.verifyEqual(trueRes, ensemble);
+        end
+	end
 end
 
 
--- matlab_code/tests/ensembleFxns/initializeEnsembleTest.m
+++ matlab_code/tests/ensembleFxns/initializeEnsembleTest.m
@@ -10,6 +10,22 @@ classdef initializeEnsembleTest < matlab.unittest.TestCase
         end
     end
     
+    methods(TestMethodTeardown)
+        function removeReactionsFolder(testCase)           
+
+            reactionsFolderList = {fullfile(testCase.currentPath{1}, '..', '..', '..', 'reactions', 'toy_model3_isoenzymes_1'), ...
+                                   fullfile(testCase.currentPath{1}, '..', '..', '..', 'reactions', 'toy_model4_1'), ...
+                                   fullfile(testCase.currentPath{1}, '..', '..', '..', 'reactions', 'toy_model4_inhibitors_1'), ...
+                                   fullfile(testCase.currentPath{1}, '..', '..', '..', 'reactions', 'toy_model1_nick_1')};
+            
+            for i = 1:size(reactionsFolderList, 2)
+                disp( exist(reactionsFolderList{i}, 'dir'));
+                if exist(reactionsFolderList{i}, 'dir')
+                    rmdir(reactionsFolderList{i}, 's');
+                end
+            end
+        end
+    end    
  
     methods (Test)
         function testInitializeEnsemble(testCase)
@@ -38,6 +54,62 @@ classdef initializeEnsembleTest < matlab.unittest.TestCase
             ensemble = initializeEnsemble(ensemble,popIdx,verbose);
             
             trueRes = load(fullfile(testCase.currentPath{1}, 'testFiles', 'trueResInitializedEnsemble_toy_model1_random.mat'));
+            testCase.verifyEqual(trueRes.ensemble,ensemble);   
+            
+        end
+        
+        function testInitializeEnsembleIsoenzymes(testCase)
+            
+            filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model3_isoenzymes');
+            popIdx = 1;
+            verbose = 1;
+            
+            ensemble = loadEnsembleStructure(filepath);   
+            ensemble = initializeEnsemble(ensemble,popIdx,verbose);
+            
+            trueRes = load(fullfile(testCase.currentPath{1}, 'testFiles', 'trueResInitializedEnsemble_toy_model3_isoenzymes.mat'));
+            testCase.verifyEqual(trueRes.ensemble,ensemble);   
+            
+        end
+         
+        function testInitializeEnsembleToyModel4(testCase)
+            
+            filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model4');
+            popIdx = 1;
+            verbose = 1;
+            
+            ensemble = loadEnsembleStructure(filepath);   
+            ensemble = initializeEnsemble(ensemble,popIdx,verbose);
+
+            trueRes = load(fullfile(testCase.currentPath{1}, 'testFiles', 'trueResInitializedEnsemble_toy_model4.mat'));
+            testCase.verifyEqual(trueRes.ensemble,ensemble);   
+            
+        end
+        
+        function testInitializeEnsembleToyModel4Inhibitors(testCase)
+            
+            filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model4_inhibitors');
+            popIdx = 1;
+            verbose = 1;
+            
+            ensemble = loadEnsembleStructure(filepath);   
+            ensemble = initializeEnsemble(ensemble,popIdx,verbose);
+
+            trueRes = load(fullfile(testCase.currentPath{1}, 'testFiles', 'trueResInitializedEnsemble_toy_model4_inhibitors.mat'));
+            testCase.verifyEqual(trueRes.ensemble,ensemble);   
+            
+        end
+        
+        function testInitializeEnsembleToyModel1Nick(testCase)
+            
+            filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_nick');
+            popIdx = 1;
+            verbose = 1;
+            
+            ensemble = loadEnsembleStructure(filepath);   
+            ensemble = initializeEnsemble(ensemble,popIdx,verbose);
+
+            trueRes = load(fullfile(testCase.currentPath{1}, 'testFiles', 'trueResInitializedEnsemble_toy_model1_nick.mat'));
             testCase.verifyEqual(trueRes.ensemble,ensemble);   
             
         end
