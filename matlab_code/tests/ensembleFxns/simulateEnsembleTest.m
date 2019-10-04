afd888508ee812dac100a2641b8ae2cc58c54831
martamatos <marta.ra.matos@gmail.com>
2019-10-04 16:54:58 +0200

    Feat: initial metabolite concentrations for simulations can be either absolute or relative

--- matlab_code/tests/ensembleFxns/simulateEnsembleTest.m
+++ matlab_code/tests/ensembleFxns/simulateEnsembleTest.m
@@ -94,9 +94,9 @@ classdef simulateEnsembleTest < matlab.unittest.TestCase
             trueRes = load(fullfile(testCase.currentPath{1}, 'testFiles', 'trueResSimulateEnsembleAbs'));
             trueRes = trueRes.simulationRes;
                    
+
             testCase.verifyThat(trueRes, matlab.unittest.constraints.IsEqualTo(simulationRes, ...
                 'Within', matlab.unittest.constraints.RelativeTolerance(1e-4)));
-
         end
     end
 end
