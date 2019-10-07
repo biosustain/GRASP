8602dae16403187b00a0c42f0cd8174746d2fd1e
martamatos <marta.ra.matos@gmail.com>
2019-10-07 18:05:33 +0200

    Feat: added input validation to loadEnsembleStructure and respective tests

--- matlab_code/tests/ensembleFxns/loadEnsembleStructureTest.m
+++ matlab_code/tests/ensembleFxns/loadEnsembleStructureTest.m
@@ -67,6 +67,128 @@ classdef loadEnsembleStructureTest < matlab.unittest.TestCase
             
             testCase.verifyEqual(trueRes.ensemble,ensemble);            
         end
+       
+        function testLoadEnsembleStructureInputValidationGeneral(testCase)
+            
+            xlsxFile = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_input_valid_0');
+            testCase.verifyError(@()loadEnsembleStructure(xlsxFile), '');            
+            
+        end
+        
+        function testLoadEnsembleStructureInputValidationRxns(testCase)
+            
+            xlsxFile = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_input_valid_1');
+            testCase.verifyError(@()loadEnsembleStructure(xlsxFile), '');            
+            
+        end
+        
+        function testLoadEnsembleStructureInputValidationRxnsRows(testCase)
+            
+            xlsxFile = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_input_valid_1_row');
+            testCase.verifyError(@()loadEnsembleStructure(xlsxFile), '');            
+            
+        end
+        
+        function testLoadEnsembleStructureInputValidationMets(testCase)
+            
+            xlsxFile = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_input_valid_2');
+            testCase.verifyError(@()loadEnsembleStructure(xlsxFile), '');            
+            
+        end
+        
+        function testLoadEnsembleStructureInputValidationMetsRows(testCase)
+            
+            xlsxFile = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_input_valid_2_row');
+            testCase.verifyError(@()loadEnsembleStructure(xlsxFile), '');            
+            
+        end
+        
+        function testLoadEnsembleStructureInputValidationMeasRates(testCase)
+            
+            xlsxFile = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_input_valid_3');
+            testCase.verifyError(@()loadEnsembleStructure(xlsxFile), '');            
+            
+        end
+        
+        function testLoadEnsembleStructureInputValidationThermoRxns(testCase)
+            
+            xlsxFile = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_input_valid_4');
+            testCase.verifyError(@()loadEnsembleStructure(xlsxFile), '');            
+            
+        end
+
+        function testLoadEnsembleStructureInputValidationThermoRxnsRows(testCase)
+            
+            xlsxFile = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_input_valid_4_row');
+            testCase.verifyError(@()loadEnsembleStructure(xlsxFile), '');            
+            
+        end
+        
+        function testLoadEnsembleStructureInputValidationThermoMets(testCase)
+            
+            xlsxFile = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_input_valid_5');
+            testCase.verifyError(@()loadEnsembleStructure(xlsxFile), '');            
+            
+        end
+        
+        function testLoadEnsembleStructureInputValidationThermoMetsRows(testCase)
+            
+            xlsxFile = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_input_valid_5_row');
+            testCase.verifyError(@()loadEnsembleStructure(xlsxFile), '');            
+            
+        end
+        
+        function testLoadEnsembleStructureInputValidationMetsData(testCase)
+            
+            xlsxFile = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_input_valid_6');
+            testCase.verifyError(@()loadEnsembleStructure(xlsxFile), '');            
+            
+        end
+
+        function testLoadEnsembleStructureInputValidationMetsDataRow(testCase)
+            
+            xlsxFile = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_input_valid_6_row');
+            testCase.verifyError(@()loadEnsembleStructure(xlsxFile), '');            
+            
+        end
+        
+        function testLoadEnsembleStructureInputValidationProtData(testCase)
+            
+            xlsxFile = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_input_valid_7');
+            testCase.verifyError(@()loadEnsembleStructure(xlsxFile), '');            
+            
+        end
+        
+        function testLoadEnsembleStructureInputValidationProtDataRows(testCase)
+            
+            xlsxFile = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_input_valid_7_row');
+            testCase.verifyError(@()loadEnsembleStructure(xlsxFile), '');            
+            
+        end
+        
+        function testLoadEnsembleStructureInputValidationKinetics(testCase)
+            
+            xlsxFile = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_input_valid_8');
+            testCase.verifyError(@()loadEnsembleStructure(xlsxFile), '');            
+            
+        end
+        
+        function testLoadEnsembleStructureInputValidationKineticsRow(testCase)
+            
+            xlsxFile = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_input_valid_8_row');
+            testCase.verifyError(@()loadEnsembleStructure(xlsxFile), '');            
+            
+        end
+        
+        function testLoadEnsembleStructureInputValidationMetsActive(testCase)
+            
+            xlsxFile = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model4');
+            ensemble = loadEnsembleStructure(xlsxFile);
+            save(fullfile(testCase.currentPath{1}, 'testFiles', 'trueResLoadEnsemble_toy_model4.mat'), 'ensemble');
+            trueRes = load(fullfile(testCase.currentPath{1}, 'testFiles', 'trueResLoadEnsemble_toy_model4.mat'));
+            
+            testCase.verifyEqual(trueRes.ensemble,ensemble);        
+        end
     end
 end
 
