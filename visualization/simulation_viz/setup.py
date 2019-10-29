15673c72721f25bca7d226fd03b3b0fc2359b428
martamatos <marta.ra.matos@gmail.com>
2019-10-29 17:34:36 +0100

    Chore: changed simulation_viz package structure and added setup file

---
+++ visualization/simulation_viz/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='simulation_viz',
+    packages=find_packages(),
+    version='0.1.0',
+    description='To plot the simulation results from GRASP.',
+    author='martamatos',
+    license='GPLv3',
+)
