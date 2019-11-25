ea45cfa926feecec8424231641d95659cfb264b3
de-leeuw <marlee@nnfcb-l0445-4.eduroam.wireless.dtu.dk>
2019-11-25 14:25:17 +0100

    Fix: Fixed plotControlAndResponseAnalysis categories

--- matlab_code/ensembleFxns/plotControlAndResponseAnalysis.m
+++ matlab_code/ensembleFxns/plotControlAndResponseAnalysis.m
@@ -1,4 +1,4 @@
-function plotControlAndResponseAnalysis(mcaResults, ensemble)
+function plotControlAndResponseAnalysis(mcaResults, ensemble, categories, enzymeCategories)
 % Plots the results from the control and response analysis, four subplots 
 % are generated: one for flux control coefficients, one for concentration
 % control, one for enzyme response coefficients, and one for concentration
@@ -53,60 +53,72 @@ ix_mets      = 1:numel(ensemble.metsActive);
 metNames     = ensemble.mets(ensemble.metsActive);
 rxnNames     = ensemble.rxns;
 
+% Checks whether any categories were defined
+if isempty(categories)
+    categories = {'ControlAndResponse_reactions',[1, length(rxnNames)]};
+end
+
+if isempty(enzymeCategories)
+    enzymeCategories = {'ControlAndResponse_enzymes',[1, length(mcaResults.enzNames)]};
+end
 
 for ix = 1:nCondition
     % Plot final results
-    figure (ix)
+    for j = 1:size(categories,1)
+    	figure ('Name', [categories{j,1} ' , condition: ' num2str(ix)])
 
-    subplot(4,1,1)
-    imagesc(mcaResults.vControlAvg{ix})
-    set(gca,'xticklabel',[],'yticklabel',[],'xtick',1:numFluxes,'ytick',1:numFluxes)
-    set(gca,'xticklabel',rxnNames,'yticklabel',rxnNames)
-    xlabel('Reactions')
-    ylabel('Reactions')
-    title(['Flux control coefficients condition: ',num2str(ix)])
-    set(gca,'FontSize',6,'FontName','arial')
-    caxis([-3 3])
-    ax=subplot(4,1,2);
-    colormap(ax,cmap)
-    
-    subplot(4,1,2)
-    imagesc(mcaResults.xControlAvg{ix})
-    set(gca,'xticklabel',[],'yticklabel',[],'ytick',1:numel(ix_mets),'xtick',1:numFluxes)
-    set(gca,'yticklabel',metNames,'xticklabel',rxnNames)
-    xlabel('Reactions')
-    ylabel('Metabolites')
-    title(['Concentration control coefficients condition: ',num2str(ix)])
-    set(gca,'FontSize',6,'FontName','arial')
-    caxis([-3 3])
-    ax = subplot(4,1,1);
-    colormap(ax,cmap)
-    colormap(cmap)
-    
-    
-    subplot(4,1,3)
-    imagesc(mcaResults.eResponseAvg{ix})
-    set(gca,'xticklabel',[],'yticklabel',[],'xtick',1:size(mcaResults.eResponseAvg{ix},2),'ytick',1:numFluxes)
-    set(gca,'xticklabel',mcaResults.enzNames,'yticklabel',rxnNames)
-    xlabel('Enzymes')
-    ylabel('Reactions')
-    title(['Enzyme control coefficients condition: ',num2str(ix)])
-    set(gca,'FontSize',6,'FontName','arial')
-    caxis([-3 3])
-    ax=subplot(4,1,3);
-    colormap(ax,cmap)
+        subplot(2,1,1)
+        imagesc(mcaResults.vControlAvg{ix}(:,categories{j,2}(1):categories{j,2}(2)))
+        set(gca,'xticklabel',[],'yticklabel',[],'xtick',1:numFluxes,'ytick',1:numFluxes)
+        set(gca,'xticklabel',rxnNames(categories{j,2}(1):categories{j,2}(2)),'yticklabel',rxnNames)
+        xlabel('Reactions')
+        ylabel('Reactions')
+        title(['Flux control coefficients condition: ',num2str(ix)])
+        set(gca,'FontSize',6,'FontName','arial')
+        caxis([-3 3])
+        ax=subplot(2,1,2);
+        colormap(ax,cmap)
 
-    subplot(4,1,4)
-    imagesc(mcaResults.xResponseAvg{ix})
-    set(gca,'xticklabel',[],'yticklabel',[],'xtick',1:size(mcaResults.xResponseAvg{ix},2),'ytick',1:numFluxes)
-    set(gca,'xticklabel',mcaResults.enzNames,'yticklabel',metNames)
-    xlabel('Enzymes')
-    ylabel('Metabolites')
-    title(['Enzyme control coefficients condition: ',num2str(ix)])
-    set(gca,'FontSize',6,'FontName','arial')
-    caxis([-3 3])
-    ax=subplot(4,1,4);
-    colormap(ax,cmap)
+        subplot(2,1,2)
+        imagesc(mcaResults.xControlAvg{ix}(categories{j,2}(1):categories{j,2}(2)))
+        set(gca,'xticklabel',[],'yticklabel',[],'ytick',1:numel(ix_mets),'xtick',1:numFluxes)
+        set(gca,'yticklabel',metNames,'xticklabel',rxnNames(categories{j,2}(1):categories{j,2}(2)))
+        xlabel('Reactions')
+        ylabel('Metabolites')
+        title(['Concentration control coefficients condition: ',num2str(ix)])
+        set(gca,'FontSize',6,'FontName','arial')
+        caxis([-3 3])
+        ax = subplot(2,1,1);
+        colormap(ax,cmap)
+        colormap(cmap)
+    end
+    
+    for l = 1:size(enzymeCategories,1)
+        figure ('Name', [enzymeCategories{l,1} ' , condition: ' num2str(ix)])
+        subplot(2,1,1)
+        imagesc(mcaResults.eResponseAvg{ix}(enzymeCategories{j,2}(1):enzymeCategories{j,2}(2)))
+        set(gca,'xticklabel',[],'yticklabel',[],'xtick',1:size(mcaResults.eResponseAvg{ix},2),'ytick',1:numFluxes)
+        set(gca,'xticklabel',mcaResults.enzNames(enzymeCategories{j,2}(1):enzymeCategories{j,2}(2)),'yticklabel',rxnNames)
+        xlabel('Enzymes')
+        ylabel('Reactions')
+        title(['Enzyme control coefficients condition: ',num2str(ix)])
+        set(gca,'FontSize',6,'FontName','arial')
+        caxis([-3 3])
+        ax=subplot(2,1,1);
+        colormap(ax,cmap)
+        mcaResults.enzNames
+        subplot(2,1,2)
+        imagesc(mcaResults.xResponseAvg{ix}(enzymeCategories{j,2}(1):enzymeCategories{j,2}(2)))
+        set(gca,'xticklabel',[],'yticklabel',[],'xtick',1:size(mcaResults.xResponseAvg{ix},2),'ytick',1:numFluxes)
+        set(gca,'xticklabel',mcaResults.enzNames(enzymeCategories{j,2}(1):enzymeCategories{j,2}(2)),'yticklabel',metNames)
+        xlabel('Enzymes')
+        ylabel('Metabolites')
+        title(['Enzyme control coefficients condition: ',num2str(ix)])
+        set(gca,'FontSize',6,'FontName','arial')
+        caxis([-3 3])
+        ax=subplot(2,1,2);
+        colormap(ax,cmap)
+    end
 
 end
 
