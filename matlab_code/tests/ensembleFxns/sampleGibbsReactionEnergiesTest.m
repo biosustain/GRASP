45242cd9c490c5e56b4fc09d309553bd7aaa343d
martamatos <marta.ra.matos@gmail.com>
2020-06-25 15:48:47 +0200

    Feat: linprog can now be used to sampleGibbsReactionEnergies

--- matlab_code/tests/ensembleFxns/sampleGibbsReactionEnergiesTest.m
+++ matlab_code/tests/ensembleFxns/sampleGibbsReactionEnergiesTest.m
@@ -12,7 +12,7 @@ classdef sampleGibbsReactionEnergiesTest < matlab.unittest.TestCase
     
  
     methods (Test)
-        function testSampleGibbsReactionEnergies1(testCase)
+        function testSampleGibbsReactionEnergiesGurobi(testCase)
             
             seed = 1;
             rng(seed)
@@ -21,17 +21,50 @@ classdef sampleGibbsReactionEnergiesTest < matlab.unittest.TestCase
             ensemble = ensemble.ensemble;
             models(1).poolFactor = [];
             strucIdx = 1;
+            
+            solver  = 'gurobi';
+            ensemble.LPSolver = solver;            
  
             [ensemble, models] = sampleGibbsReactionEnergies(ensemble, models, strucIdx);
 
             trueRes = load(fullfile(testCase.currentPath{1}, 'testFiles', 'trueResSampleGibbsReactionEnergies1'));
             trueResModels = trueRes.models;
             trueResEnsemble = trueRes.ensemble;
-                   
-            testCase.verifyEqual(trueResModels, models);
-            testCase.verifyEqual(trueResEnsemble, ensemble);
+            trueResEnsemble.LPSolver = solver;
+           
+            testCase.verifyThat(trueResModels, matlab.unittest.constraints.IsEqualTo(models, ...
+                 'Within', matlab.unittest.constraints.RelativeTolerance(1e-12)));
+            
+            testCase.verifyThat(trueResEnsemble, matlab.unittest.constraints.IsEqualTo(ensemble, ...
+                 'Within', matlab.unittest.constraints.RelativeTolerance(1e-12)));
         end
        
+        function testSampleGibbsReactionEnergiesLinprog(testCase)
+            
+            seed = 1;
+            rng(seed)
+           
+            ensemble = load(fullfile(testCase.currentPath{1}, 'testFiles', 'initializedEnsemble_toy_model1_measuredMets'));
+            ensemble = ensemble.ensemble;
+            models(1).poolFactor = [];
+            strucIdx = 1;
+            
+            solver  = 'linprog';
+            ensemble.LPSolver = solver;            
+ 
+            [ensemble, models] = sampleGibbsReactionEnergies(ensemble, models, strucIdx);
+
+            trueRes = load(fullfile(testCase.currentPath{1}, 'testFiles', 'trueResSampleGibbsReactionEnergies1'));
+            trueResModels = trueRes.models;
+            trueResEnsemble = trueRes.ensemble;
+            trueResEnsemble.LPSolver = solver;
+           
+            testCase.verifyThat(trueResModels, matlab.unittest.constraints.IsEqualTo(models, ...
+                 'Within', matlab.unittest.constraints.RelativeTolerance(1e-12)));
+            
+            testCase.verifyThat(trueResEnsemble, matlab.unittest.constraints.IsEqualTo(ensemble, ...
+                 'Within', matlab.unittest.constraints.RelativeTolerance(1e-12)));
+        end
     end
 end
 
