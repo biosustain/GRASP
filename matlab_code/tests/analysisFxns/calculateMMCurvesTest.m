913393d6a86bce3590ff2441645ff37430c35dc9
martamatos <marta.ra.matos@gmail.com>
2021-03-20 16:29:30 +0100

    tests: Use readmatrix instead of fileread to read files
    This is in the test to calculate MM curves,
    comparing strings was a terrible idea due to numerical
    precisions issues.

--- matlab_code/tests/analysisFxns/calculateMMCurvesTest.m
+++ matlab_code/tests/analysisFxns/calculateMMCurvesTest.m
@@ -34,14 +34,14 @@ classdef calculateMMCurvesTest < matlab.unittest.TestCase
             calculateMMCurves(outputFolder, testCase.ensemble, numModels, structIdx, saturatingConc, substrateRange, rxnList);
             
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'putida_OP2_r_R_GTHOr_m_m_gthox_c.csv');
-            resGthox = fileread(filepath);
+            resGthox = readmatrix(filepath);
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'putida_OP2_r_R_GTHOr_m_m_nadph_c.csv');
-            resNadph = fileread(filepath);
+            resNadph = readmatrix(filepath);
             
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'OP2_GTHOr_gthox.csv');
-            trueResGthox = fileread(filepath);
+            trueResGthox = readmatrix(filepath);
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'OP2_GTHOr_nadph.csv');
-            trueResNadph = fileread(filepath);
+            trueResNadph = readmatrix(filepath);
             
             testCase.verifyThat(resGthox, matlab.unittest.constraints.IsEqualTo(trueResGthox, ...
                 'Within', matlab.unittest.constraints.RelativeTolerance(testCase.relTol) | matlab.unittest.constraints.AbsoluteTolerance(testCase.absTol)));  
@@ -59,14 +59,14 @@ classdef calculateMMCurvesTest < matlab.unittest.TestCase
             calculateMMCurves(outputFolder, testCase.ensemble, numModels, structIdx, saturatingConc, substrateRange, rxnList);
             
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'putida_OP2_r_R_GTHPi_m_m_gthrd_c.csv');
-            resGthrd = fileread(filepath);
+            resGthrd = readmatrix(filepath);
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'putida_OP2_r_R_GTHPi_m_m_h2o2_c.csv');
-            resH2o2 = fileread(filepath);
+            resH2o2 = readmatrix(filepath);
             
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'OP2_GTHPi_gthrd.csv');
-            trueResGthrd = fileread(filepath);
+            trueResGthrd = readmatrix(filepath);
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'OP2_GTHPi_h2o2.csv');
-            trueResH2o2 = fileread(filepath);
+            trueResH2o2 = readmatrix(filepath);
             
             testCase.verifyThat(resGthrd, matlab.unittest.constraints.IsEqualTo(trueResGthrd, ...
                 'Within', matlab.unittest.constraints.RelativeTolerance(testCase.relTol) | matlab.unittest.constraints.AbsoluteTolerance(testCase.absTol)));  
@@ -85,15 +85,15 @@ classdef calculateMMCurvesTest < matlab.unittest.TestCase
             
             
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'putida_OP2_r_R_G6PDH2_NAD_m_m_g6p_c.csv');
-            resG6p = fileread(filepath);
+            resG6p = readmatrix(filepath);
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'putida_OP2_r_R_G6PDH2_NAD_m_m_nad_c.csv');
-            resNad = fileread(filepath);
+            resNad = readmatrix(filepath);
             
             
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'OP2_G6PDH2_NAD_g6p.csv');
-            trueResG6p = fileread(filepath);
+            trueResG6p = readmatrix(filepath);
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'OP2_G6PDH2_NAD_nad.csv');
-            trueResNad = fileread(filepath);
+            trueResNad = readmatrix(filepath);
             
             testCase.verifyThat(resG6p, matlab.unittest.constraints.IsEqualTo(trueResG6p, ...
                 'Within', matlab.unittest.constraints.RelativeTolerance(testCase.relTol) | matlab.unittest.constraints.AbsoluteTolerance(testCase.absTol)));  
@@ -111,24 +111,24 @@ classdef calculateMMCurvesTest < matlab.unittest.TestCase
             calculateMMCurves(outputFolder, testCase.ensemble, numModels, structIdx, saturatingConc, substrateRange, rxnList);
             
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'putida_OP2_r_R_G6PDH2_NADP_m_m_g6p_c.csv');
-            resG6p = fileread(filepath);
+            resG6p = readmatrix(filepath);
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'putida_OP2_r_R_G6PDH2_NADP_m_m_nadp_c.csv');
-            resNadp = fileread(filepath);
+            resNadp = readmatrix(filepath);
             
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'putida_OP2_r_R_G6PDH2_NADP_m_m_6pgl_c.csv');
-            res6PGL = fileread(filepath);
+            res6PGL = readmatrix(filepath);
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'putida_OP2_r_R_G6PDH2_NADP_m_m_nadph_c.csv');
-            resNadph = fileread(filepath);
+            resNadph = readmatrix(filepath);
             
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'OP2_G6PDH2_NADP_g6p.csv');
-            trueResG6p = fileread(filepath);
+            trueResG6p = readmatrix(filepath);
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'OP2_G6PDH2_NADP_nadp.csv');
-            trueResNadp = fileread(filepath);
+            trueResNadp = readmatrix(filepath);
             
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'OP2_G6PDH2_NADP_6pgl.csv');
-            trueRes6PGL = fileread(filepath);
+            trueRes6PGL = readmatrix(filepath);
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'OP2_G6PDH2_NADP_nadph.csv');
-            trueResNadph = fileread(filepath);
+            trueResNadph = readmatrix(filepath);
 
             testCase.verifyThat(resG6p, matlab.unittest.constraints.IsEqualTo(trueResG6p, ...
                 'Within', matlab.unittest.constraints.RelativeTolerance(testCase.relTol) | matlab.unittest.constraints.AbsoluteTolerance(testCase.absTol)));  
@@ -155,24 +155,24 @@ classdef calculateMMCurvesTest < matlab.unittest.TestCase
             calculateMMCurves(outputFolder, ensembleLocal, numModels, structIdx, saturatingConc, substrateRange);
             
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model_r_r1_m_m3.csv');
-            resR1M3 = fileread(filepath);
+            resR1M3 = readmatrix(filepath);
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model_r_r1_m_m6.csv');
-            resR1M6 = fileread(filepath);
+            resR1M6 = readmatrix(filepath);
             
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model_r_r3_m_m1.csv');
-            resR3M1 = fileread(filepath);
+            resR3M1 = readmatrix(filepath);
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model_r_r3_m_m7.csv');
-            resR3M7 = fileread(filepath);
+            resR3M7 = readmatrix(filepath);
             
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'trueRes_r_r1_m_m3.csv');
-            trueResR1M3 = fileread(filepath);
+            trueResR1M3 = readmatrix(filepath);
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'trueRes_r_r1_m_m6.csv');
-            trueResR1M6 = fileread(filepath);
+            trueResR1M6 = readmatrix(filepath);
             
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'trueRes_r_r3_m_m1.csv');
-            trueResR3M1 = fileread(filepath);
+            trueResR3M1 = readmatrix(filepath);
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'trueRes_r_r3_m_m7.csv');
-            trueResR3M7 = fileread(filepath);
+            trueResR3M7 = readmatrix(filepath);
 
             testCase.verifyThat(resR1M3, matlab.unittest.constraints.IsEqualTo(trueResR1M3, ...
                 'Within', matlab.unittest.constraints.RelativeTolerance(testCase.relTol) | matlab.unittest.constraints.AbsoluteTolerance(testCase.absTol)));  
@@ -200,34 +200,34 @@ classdef calculateMMCurvesTest < matlab.unittest.TestCase
             calculateMMCurves(outputFolder, ensembleLocal, numModels, structIdx, saturatingConc, substrateRange, rxnList);
             
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_random2_r_r1_m_m3.csv');
-            resR1M3 = fileread(filepath);
+            resR1M3 = readmatrix(filepath);
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_random2_r_r1_m_m6.csv');
-            resR1M6 = fileread(filepath);
+            resR1M6 = readmatrix(filepath);
             
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_random2_r_r3_m_m1.csv');
-            resR3M1 = fileread(filepath);
+            resR3M1 = readmatrix(filepath);
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_random2_r_r3_m_m7.csv');
-            resR3M7 = fileread(filepath);
+            resR3M7 = readmatrix(filepath);
             
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_random2_r_r6_m_m1.csv');
-            resR6M1 = fileread(filepath);
+            resR6M1 = readmatrix(filepath);
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'toy_model1_random2_r_r6_m_m10.csv');
-            resR6M10 = fileread(filepath);
+            resR6M10 = readmatrix(filepath);
             
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'trueRes_r_r1_m_m3_water.csv');
-            trueResR1M3 = fileread(filepath);
+            trueResR1M3 = readmatrix(filepath);
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'trueRes_r_r1_m_m6_water.csv');
-            trueResR1M6 = fileread(filepath);
+            trueResR1M6 = readmatrix(filepath);
             
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'trueRes_r_r3_m_m1_water.csv');
-            trueResR3M1 = fileread(filepath);
+            trueResR3M1 = readmatrix(filepath);
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'trueRes_r_r3_m_m7_water.csv');
-            trueResR3M7 = fileread(filepath);
+            trueResR3M7 = readmatrix(filepath);
             
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'trueRes_r_r6_m_m1_water.csv');
-            trueResR6M1 = fileread(filepath);
+            trueResR6M1 = readmatrix(filepath);
             filepath = fullfile(testCase.currentPath{1}, 'testFiles', 'trueRes_r_r6_m_m10_water.csv');
-            trueResR6M10 = fileread(filepath);
+            trueResR6M10 = readmatrix(filepath);
             
             testCase.verifyThat(resR1M3, matlab.unittest.constraints.IsEqualTo(trueResR1M3, ...
                 'Within', matlab.unittest.constraints.RelativeTolerance(testCase.relTol) | matlab.unittest.constraints.AbsoluteTolerance(testCase.absTol)));  
